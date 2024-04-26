32f0c42e6aa290a9f196e1df612ab2d639f6c36f
silvio <fuocosilvio>
2024-04-26 16:24:07 +0200

    commit

---
+++ src/components/Description.jsx
@@ -0,0 +1,3 @@
+export function Description({ children }) {
+  return <p>{children}</p>;
+}
--- src/pages/DetailProduitPage.jsx
+++ src/pages/DetailProduitPage.jsx
@@ -1,3 +1,52 @@
+import { Button } from "../components/Button";
+import { Card } from "../components/Card";
+import { Titre } from "../components/Titre";
+
 export function DetailProduit() {
-  return <p>detail produit</p>;
+  return (
+    <div className="container">
+      <div>
+        <div className="card-detail">
+          <img
+            src="https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+
+          <img
+            src="https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+
+          <img
+            className="n-3"
+            src="https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="detail-text">
+          <p>marque</p>
+          <p>STAINLESS STEEL WRIST WATCH 699$</p>
+          <p>description</p>
+          <p>
+            {" "}
+            Watch Crown With Environmental IPS Bronze Electroplating; Display
+            system of 12 hours
+          </p>
+          <p>Note</p>
+          <p>4.05 / 5</p>
+        </div>
+      </div>
+      <div className="detail-card">
+        <div className="text-button">
+          <Titre>dans la meme categorie</Titre>
+          <Button>TOUT VOIR</Button>
+        </div>
+        <div className="cards-container">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 }
